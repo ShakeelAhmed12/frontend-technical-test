9f6beed61899592a4a28b0168bbdd55479691d4f
Shakeel <shakeel_ahmed@live.co.uk>
2021-07-02 19:35:08 +0100

    Fetching and returning vehicle data

--- src/api/index.js
+++ src/api/index.js
@@ -8,5 +8,54 @@ import { request } from './helpers';
  */
 // TODO: All API related logic should be made inside this function.
 export default async function getData() {
-  return [];
+  // const vehiclesList = [];
+  // const endpointURLS = [];
+
+  // const initalVehicleResponse = await fetch('/api/vehicles.json');
+  // const initalVehicleData = await initalVehicleResponse.json();
+
+  // initalVehicleData.map((vehicle) => {
+  //   endpointURLS.push(vehicle.apiUrl);
+
+  //   vehiclesList.push({
+  //     vehicleId: vehicle.id,
+  //     vehicleMedia: vehicle.media
+  //   })
+  // });
+
+  // try{
+  //   let vehicleResponseData = await Promise.all(
+  //     endpointURLS.map(url => fetch(url)
+  //       .then(r => r.json())
+  //       .catch(error => ({ error, url }))
+  //     )
+  //   )
+
+  //   const vehicleData = await Promise.all(vehicleResponseData);
+
+  //   vehicleData.forEach((vehicle, i) => {
+  //     vehiclesList[i].price = vehicle.price;
+  //     vehiclesList[i].description = vehicle.description;
+  //   });
+
+  // }catch(errors){
+  //   console.log(errors);
+  // }
+
+  const vehicles = await fetch('/api/vehicles.json').then((data) => data.json());
+  try {
+    const vehicleResponses = await Promise.all(
+      vehicles.map((v) => fetch(v.apiUrl)
+        .then((r) => r.json())
+        .catch((error) => ({ error, v })))
+    );
+
+    return vehicles.map((v, i) => ({
+      ...v,
+      price: vehicleResponses[i].price ? vehicleResponses[i].price : null,
+      description: vehicleResponses[i].description ? vehicleResponses[i].description : null,
+    }));
+  } catch (errors) {
+    console.log(errors);
+  }
 }
